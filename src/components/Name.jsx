586ce20df7048f088f9dbfb4fcfd82375bbb6c52
Jagadeesh Sutar <jagadeeshsutar56883@gamil.com>
2025-04-02 03:31:38 +0530

    Cleared all the things interrupting the responsiveness

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -4,7 +4,7 @@ import "./Name.css";
 function Name() {
   return (
     <>
-      <h1 className="flex items-center justify-center h-screen bg-blue-500 text-white text-4xl font-bold nameAnimate">Jagadeesh</h1>
+      <h1 className="flex items-center justify-center h-screen bg-blue-500 text-white text-4xl font-bold nameAnimate">Jagadeesh <span>Sutar</span></h1>
     </>
   );
 }
